4ab5e6891a91275009177c8767a65df64a213540
VaishnaviSahu206 <vaishnavi.2428cseaim@kiet.edu>
2025-09-11 12:28:31 +0530

    11-09-2025

--- ArrayList10.java
+++ ArrayList10.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 public class ArrayList10 {
     public static void main(String[] args) {
         ArrayList <String> fruits=new ArrayList();
@@ -12,13 +14,36 @@ public class ArrayList10 {
         for(String s:fruits){
             System.out.println(s);
         }
-
+        System.out.println("---------------- Add at specific index ----------------");
         fruits.add(1,"Guava");
         System.out.println(fruits);
 
+        System.out.println("--------------Get-------------");
         System.out.println(fruits.get(3));
+
+        System.out.println("---------------- Update element ----------------");
         System.out.println(fruits.set(1,"Mango"));
 
+        
+        System.out.println("---------------- Remove elements ----------------");
+        fruits.remove(1); // by index
+        System.out.println("After removing index 1: " + fruits);
 
+
+         for(String s:fruits){
+            System.out.println(s);
+         }
+         Iterator itr=fruits.iterator();
+         while(itr.hasNext()){
+            System.out.println(itr.next());
+         }
+
+         Collections.sort(fruits);
+         System.out.println(fruits);
+
+         System.out.println(Collections.binarySearch(fruits,"Berry"));
+
+         fruits.clear();
+        
     }
 }
---
+++ LinkedList11.java
@@ -0,0 +1,29 @@
+import java.util.LinkedList;
+
+public class LinkedList11 {
+
+    public static void main(String[] args) {
+        LinkedList <String> employees = new LinkedList<>();
+    
+    employees.add("John");
+    employees.add(1,"Raj");
+    employees.addFirst("Sofia");
+    employees.addLast("Arjun");
+    
+    System.out.println("Employees after adding: "+ employees);
+
+    System.out.println("Get First: "+ employees.getFirst());
+    System.out.println("Get Last: "+ employees.getLast());
+    System.out.println("Get index 2: "+ employees.get(2));
+
+    employees.remove();
+    employees.remove(1);
+    employees.remove("Raj");
+    employees.removeFirst();
+    employees.removeLast();
+    System.out.println("Employees after removals: "+employees);
+
+
+    }
+
+}
